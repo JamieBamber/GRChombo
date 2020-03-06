282b7ec7d7e4cec66031886b2f0d97cc3ada3f1d
Jamie bamber <jarb2@cam.ac.uk>
2020-03-06 10:14:49 +0000

    fixed BoundaryConditions.hpp & .cpp

--- Source/GRChomboCore/BoundaryConditions.hpp
+++ Source/GRChomboCore/BoundaryConditions.hpp
@@ -34,7 +34,9 @@ class BoundaryConditions
         STATIC_BC,
         SOMMERFELD_BC,
         REFLECTIVE_BC,
-        EXTRAPOLATING_BC
+        EXTRAPOLATING_BC,
+        MIXED_BC,
+        ANALYTIC_BC
     };
 
     /// enum for possible parity states
@@ -57,7 +59,6 @@ class BoundaryConditions
         std::array<bool, CH_SPACEDIM> is_periodic;
         std::array<int, NUM_VARS> vars_parity;
         std::array<double, NUM_VARS> vars_asymptotic_values;
-        int extrapolation_order = 1;
     };
 
   protected:
@@ -150,7 +151,9 @@ class BoundaryConditions
                               const IntVect iv,
                               const Interval comps = Interval(0,
                                                               NUM_VARS)) const;
-
+    void fill_analytic_cell(FArrayBox &rhs_box, const FArrayBox &soln_box,
+                            const IntVect iv,
+                            const Interval comps = Interval(0, NUM_VARS)) const;
     void fill_reflective_cell(FArrayBox &rhs_box, const IntVect iv,
                               const Side::LoHiSide a_side, const int dir,
                               const Interval comps = Interval(0,
