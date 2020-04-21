d53f65d1c3a088c7c8f4c18a717b4a6d32748dbe
Jamie bamber <jarb2@cam.ac.uk>
2020-04-21 12:10:45 +0100

    files from supermuc

--- Examples/BinaryBHScalarField/DensityAndMom_v2.hpp
+++ Examples/BinaryBHScalarField/DensityAndMom_v2.hpp
@@ -8,10 +8,9 @@
 
 #include "CCZ4Geometry.hpp"
 #include "Cell.hpp"
-#include "Constraints.hpp"
+#include "BSSNVars.hpp"
 #include "Coordinates.hpp"
 #include "FourthOrderDerivatives.hpp"
-#include "GRInterval.hpp"
 #include "Tensor.hpp"
 #include "UserVariables.hpp" //This files needs NUM_VARS - total number of components
 #include "VarsTools.hpp"
@@ -20,40 +19,42 @@
 
 //! Calculates the density rho with type matter_t and writes it to the grid
 template <class matter_t> 
-class DensityAndMom : public Constraints
+class DensityAndMom
 {
   public:
     // Use the variable definition in the matter class
     template <class data_t>
     using MatterVars = typename matter_t::template Vars<data_t>;
 
-    // Inherit the variable definitions from CCZ4 + matter_t
+    /// CCZ4 variables
+    template <class data_t> 
+    using MetricVars = BSSNVars::VarsNoGauge<data_t>;
+
+    // Inherit the variables from MatterVars and MetricVars
     template <class data_t>
-    struct Vars : public Constraints::Vars<data_t>, public MatterVars<data_t>
-    // setting up a struct DensityAndMom::Vars which inherits from the struct CCZ4::Vars
+    struct Vars : public MetricVars<data_t>, public MatterVars<data_t>
     {
         /// Defines the mapping between members of Vars and Chombo grid
         /// variables (enum in User_Variables)
-        template <typename mapping_function_t> // this is a template function
+        template <typename mapping_function_t>
         void enum_mapping(mapping_function_t mapping_function)
         {
-            Constraints::Vars<data_t>::enum_mapping(mapping_function);
+            MetricVars<data_t>::enum_mapping(mapping_function);
             MatterVars<data_t>::enum_mapping(mapping_function);
         }
     };
 
     DensityAndMom(matter_t a_matter, double a_dx, std::array<double, CH_SPACEDIM> a_center)
-        : Constraints(a_dx, 0.0 /*No cosmological constant*/), m_matter(a_matter), m_deriv(a_dx), 
-		m_dx(a_dx), m_center(a_center)
+        : m_matter(a_matter), m_deriv(a_dx), m_dx(a_dx), m_center(a_center)
     {
     }
 
     template <class data_t> void compute(Cell<data_t> current_cell) const
     {
+	CH_TIME("DensityAndMom::compute");
         // copy data from chombo gridpoint into local variables, and derivs
 	const auto vars = current_cell.template load_vars<Vars>();
         const auto d1 = m_deriv.template diff1<Vars>(current_cell);
-
         const auto h_UU = TensorAlgebra::compute_inverse_sym(vars.h);
 	const auto chris = TensorAlgebra::compute_christoffel(d1.h, h_UU);
 
