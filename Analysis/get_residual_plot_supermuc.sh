ebf8b6da69118bbeabe0826a8ff7db1e571e130e
Jamie bamber <jarb2@cam.ac.uk>
2020-06-18 13:15:49 +0100

    new plots from supermuc

--- Analysis/get_residual_plot_supermuc.sh
+++ Analysis/get_residual_plot_supermuc.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-variable=rho
+variable=phi
 subdir=run0005_FlatScalar_mu1_G0
-folder=BBH_run0005
-number=1160
+number=1000
 echo "start scp"
 # scp di76bej@skx.supermuc.lrz.de:
-filename=BBH_SF_${variable}_${subdir}_n$(printf "%06d" ${number}).png
+filename=*_${variable}_${subdir}_n$(printf "%06d" ${number}).png
 #filename=BBH_SF_rho_test0001_n000003.png
-scp supermuc:~/GRChombo/GRChombo/Analysis/plots/Binary_BH/${folder}/${filename} ~/GRChombo/Analysis/plots/Binary_BH/${filename}
+scp supermuc:~/GRChombo/GRChombo/Analysis/plots/Binary_BH/${filename} ~/GRChombo/Analysis/plots/Binary_BH
 echo "saved ~/GRChombo/Analysis/plots/Binary_BH/${filename}"
 open ~/GRChombo/Analysis/plots/Binary_BH/${filename}
