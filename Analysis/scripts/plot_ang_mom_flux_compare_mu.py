e2a50a54b92be71025daf59126fa5c5ceba518ae
James Bamber <dc-bamb1@login-e-16.data.cluster>
2020-09-11 14:34:23 +0100

    new plots

--- Analysis/scripts/plot_ang_mom_flux_compare_mu.py
+++ Analysis/scripts/plot_ang_mom_flux_compare_mu.py
@@ -91,7 +91,7 @@ class data_dir:
 		self.tflux = flux_data[1:,0]
 		self.r_min = flux_data[0,1]
 		self.r_max = flux_data[0,2]
-		E0 = 0.5*(4*np.pi*(self.r_max**3)/3)*(phi0*mu)**2
+		E0 = 0.5*(4*np.pi*(self.r_max**3)/3)*(phi0)**2
 		self.inner_ang_mom_flux = -flux_data[1:,1]/E0
 		self.outer_ang_mom_flux = -flux_data[1:,2]/E0	
 		if cumulative:
@@ -135,15 +135,18 @@ run0016_l1_m-1_a0.99_Al0_mu0.4_M1_IsoKerr
 run0017_l1_m1_a0.99_Al0.5_mu0.4_M1_IsoKerr
 run0018_l1_m1_a0.99_Al0.25_mu0.4_M1_IsoKerr"""
 
-add_data_dir(2, 0, 0, "0.7", "0.4", "0", 64, 18, "_theta_max1.0")
+#add_data_dir(2, 0, 0, "0.7", "0.4", "0", 64, 18, "_theta_max1.0")
+#add_data_dir(5, 1, 1, "0.7", "0.4", "0", 64, 18, "_theta_max1.0")
+#add_data_dir(7, 2, 2, "0.7", "0.4", "0", 64, 18, "_theta_max1.0")
+#add_data_dir(8, 4, 4, "0.7", "0.4", "0", 64, 18, "_theta_max1.0")
+#add_data_dir(9, 1, -1, "0.7", "0.4", "0", 64, 18, "_theta_max1.0")
+#add_data_dir(10, 8, 8, "0.7", "0.4", "0", 64, 18, "_theta_max1.0")
+
 add_data_dir(5, 1, 1, "0.7", "0.4", "0", 64, 18, "_theta_max1.0")
-add_data_dir(7, 2, 2, "0.7", "0.4", "0", 64, 18, "_theta_max1.0")
-add_data_dir(8, 4, 4, "0.7", "0.4", "0", 64, 18, "_theta_max1.0")
-add_data_dir(9, 1, -1, "0.7", "0.4", "0", 64, 18, "_theta_max1.0")
-add_data_dir(10, 8, 8, "0.7", "0.4", "0", 64, 18, "_theta_max1.0")
+add_data_dir(11, 1, 1, "0.7", "2.0", "0", 64, 18, "_theta_max1.0")
+add_data_dir(20, 1, 1, "0.7", "0.1", "0", 64, 18, "_theta_max1.0")
 
 #add_data_dir(15, 1, 1, "0.7", "0.4", "0.5", 64, 18, "_theta_max1.0")
-
 #add_data_dir(6, 1, 1, "0.99", "0.4", "0", 64, 18, "_theta_max1.0")
 #add_data_dir(16, 1, -1, "0.99", "0.4", "0", 64, 18, "_theta_max1.0")
 #add_data_dir(17, 1, 1, "0.99", "0.4", "0.5", 64, 18, "_theta_max1.0")
@@ -168,8 +171,8 @@ def plot_graph():
 		dd.load_data()
 		mu = float(dd.mu)
 		#net_flux = outer_ang_mom_flux - inner_ang_mom_flux
-		#label_ = "$\\mu$={:.2f}".format(mu)
-		label_ = "$l$={:d} $m$={:d}".format(dd.l, dd.m)
+		label_ = "$\\mu$={:.2f}".format(mu)
+		#label_ = "$l$={:d} $m$={:d}".format(dd.l, dd.m)
 		#label_ = "$m$={:d} $\\alpha$={:s}".format(dd.m, dd.Al)
 		#ax1.plot(dd.tflux,dd.inner_ang_mom_flux,colours[i]+":", label="flux into $R_+$ "+label_)
 		ax1.plot(dd.tflux*mu,dd.outer_ang_mom_flux,colours[i]+"-", label=label_, linewidth=1)
@@ -183,15 +186,15 @@ def plot_graph():
 		i = i + 1
 	ax1.set_xlabel("$\\tau$", fontsize=label_size)
 	ax1.set_xlim((0, 300))
-	ax1.set_ylim((-0.7, 0.25))
+	ax1.set_ylim((-0.002, 0.01))
 	if cumulative:
-		ax1.set_ylabel("cumulative flux / $E_0$", fontsize=label_size)
-		ax1.set_title("Cumulative ang. mom. flux, $M=1$, $\\chi=0.7$, $\\mu=0.4$", wrap=True, fontsize=title_font_size)
-		save_path = home_path + "plots/ang_mom_flux_in_R{:.0f}_IsoKerr_compare_lm_cumulative.png".format(R_max)
+		ax1.set_ylabel("cumulative flux / $V_0 \\frac{1}{2} \\varphi^2_0$")
+		plt.title("Cumulative ang. mom. flux, $M=1$, \n $a=0.7$, $l=m=1$", fontsize=title_font_size)
+		save_path = home_path + "plots/ang_mom_flux_in_R{:.0f}_IsoKerr_compare_mu_cumulative.png".format(R_max)
 	else:
-		ax1.set_ylabel("flux / $E_0$", fontsize=label_size)
-		plt.title("Mass flux, $M=1$, $\\chi=0.7$, $\\mu=0.4$")
-		save_path = home_path + "plots/ang_mom_flux_in_R{:.0f}_IsoKerr_compare_lm.png".format(R_max)
+		ax1.set_ylabel("flux / $E_0$")
+		plt.title("Ang. mom. flux, $M=1$, $a=0.7$, $l=m=1$", fontsize=title_font_size)
+		save_path = home_path + "plots/ang_mom_flux_in_R{:.0f}_IsoKerr_compare_mu.png".format(R_max)
 	ax1.legend(loc='best', fontsize=legend_font_size)
 	plt.xticks(fontsize=font_size)
 	plt.yticks(fontsize=font_size)
