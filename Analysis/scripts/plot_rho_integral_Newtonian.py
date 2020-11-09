f984d2bfc535bd8466aeabaeb2cbb219ed6580b6
James Bamber <dc-bamb1@login5b.pri.cosma7.alces.network>
2020-11-09 15:15:00 +0000

    plotting file for Newtonian BH

--- Analysis/scripts/plot_rho_integral_Newtonian.py
+++ Analysis/scripts/plot_rho_integral_Newtonian.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt
 
 # set up parameters
 data_root_path = "/cosma6/data/dp174/dc-bamb1/GRChombo_data/NewtonianBinaryBHScalar/"
-home_path="/cosma/home/dp174/dc-bamb1/GRChombo/Analysis/plots"
+home_path="/cosma/home/dp174/dc-bamb1/GRChombo/Analysis/"
 
 half_box = True
 
@@ -23,7 +23,7 @@ Theta_max="1.0"
 Ntheta=18
 Nphi=64
 
-r_max = 
+r_max = 120
 
 class data_dir:
 	def __init__(self, num, M, d, mu, dt):
@@ -32,18 +32,18 @@ class data_dir:
 		self.d = d
 		self.mu = mu
 		self.dt = dt
-		self.name = "run{:04d}_M{:s}_d{:s}_mu{:s}_dt_mult{:s}".format(M, d, mu, dt)
+		self.name = "run{:04d}_M{:s}_d{:s}_mu{:s}_dt_mult{:s}".format(num, M, d, mu, dt)
 	#
 	def load_data(self):
 		# load flux and mass data from csv files	
 		file_name = data_root_path + self.name + "/outputs/" + "RhoIntegral.dat"
 		data = np.genfromtxt(file_name, skip_header=1)
 		print("loaded " + file_name)
-		self.time = flux_data[:,0]
+		self.time = data[:,0]
 		mu = float(self.mu)
-		E0 = 0.5*(4*np.pi*(self.r_max**3)/3)*(phi0*mu)**2
-		self.rho_integral = (flux_data[:,1] - flux_data[0,1])/E0
-		self.rhoJ_integral = (flux_data[:,2] - flux_data[0,2])/E0
+		E0 = 0.5*(4*np.pi*(r_max**3)/3)*(phi0*mu)**2
+		self.rho_integral = (data[:,1] - data[0,1])/E0
+		self.rhoJ_integral = (data[:,2] - data[0,2])/E0
 				
 data_dirs = []
 def add_data_dir(num, M, d, mu, dt):
@@ -74,15 +74,15 @@ def plot_graph():
 	for dd in data_dirs:
 		dd.load_data()
 		mu = float(dd.mu)
-		label_ = "$\\mu$={:s} $m$={:d}".format(dd.l, dd.m)
+		label_ = "$\\mu$={:s}".format(dd.mu)
 		ax1.plot(dd.time,dd.rho_integral,colours[i]+"-", label=label_, linewidth=1)
 		i = i + 1
 	ax1.set_xlabel("$t$", fontsize=label_size)
 	#ax1.set_xlim((0, 500))
 	#ax1.set_ylim((-0.15, 0.15))
 	ax1.set_ylabel("$change in \\rho$ integral / $E_0$", fontsize=label_size)
-	ax1.set_title("Change in integrated density, $M=0.1,d=10", wrap=True, fontsize=title_font_size)
-	save_path = home_path + "plots/Newtonian_Scalar_change_in_rho_integral_vs_mu.png".format(R_max)
+	ax1.set_title("Change in integrated density, $M=0.1,d=10$", wrap=True, fontsize=title_font_size)
+	save_path = home_path + "/plots/Newtonian_Scalar_change_in_rho_integral_vs_mu.png".format(R_max)
 	ax1.legend(loc='upper left', ncol=2, fontsize=legend_font_size)
 	plt.xticks(fontsize=font_size)
 	plt.yticks(fontsize=font_size)
